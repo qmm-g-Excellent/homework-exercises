9bd1a4e3a05b053b026760cf3c8b854ad71fc767
Minmin Qiu <mmqiu@thoughtworks.com>
2018-02-28 17:53:39 +0800

    complete FizzBuzz Exercise

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,21 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number:");
-        Integer n = scanner.nextInt();
-
-        for (int i = 0; i < 2 * n - 1; i++) {
-
-            if(i<n){
-                outputLineEmpty(n - 1 - i);
-                outputLineStar(2 * i + 1);
-            }
-
-
-            if(i>=n){
-
-                outputLineEmpty(i-(n-1));
-                outputLineStar(2*n-1-2*(i-n+1));
-            }
-            System.out.println();
-        }
-
+        FizzBuzz();
     }
 
-    private static void outputLineEmpty(int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(" ");
-        }
-    }
-
-    private static void outputLineStar(int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print("*");
+    private static void FizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 
